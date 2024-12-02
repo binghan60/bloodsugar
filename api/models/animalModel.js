14c85b9f93e28626f8626dd8affb4e4c7b54cc9f
binghan60 <binghan60@gmail.com>
2024-12-02 12:06:04 +0800

    動物資料庫與schema

---
+++ api/models/animalModel.js
@@ -0,0 +1,33 @@
+﻿import mongoose from 'mongoose';
+
+const animalSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true }, // 名字（必填）
+		birthday: { type: Date }, // 生日（使用 Date 類型）
+		bloodType: {
+			type: String,
+			enum: ['A', 'B', 'AB', 'O', 'Unknown'], // 限制血型類型
+			default: 'Unknown',
+		},
+		variety: { type: String }, // 品種
+		type: {
+			type: String,
+			enum: ['Dog', 'Cat', 'Other'], // 限制動物類型
+			required: true,
+		},
+		gender: {
+			type: String,
+			enum: ['Male', 'Female', 'Unknown'], // 限制性別
+			default: 'Unknown',
+		},
+		weight: { type: Number, default: 0 }, // 體重
+		ligation: { type: Boolean, default: false }, // 是否結紮
+	},
+	{
+		timestamps: true, // 自動生成 createdAt 和 updatedAt
+	}
+);
+
+const Animal = mongoose.model('Animal', animalSchema);
+
+export default Animal;
---
+++ api/routes/animal.js
@@ -0,0 +1,37 @@
+﻿import express from 'express';
+import Animal from '../models/animalModel.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+	res.send('ANIMAL');
+});
+
+router.post('/create', async (req, res) => {
+	const newAnimal = new Animal({
+		//以req.body的資料new一個User
+		name: req.body.name,
+		birthday: req.body.birthday,
+		bloodType: req.body.bloodType,
+		variety: req.body.variety,
+		type: req.body.type,
+		gender: req.body.gender,
+		weight: req.body.weight,
+		ligation: req.body.ligation,
+	});
+	const animal = await newAnimal.save(); //保存至資料庫
+	res.send({
+		//通過驗證回傳帳號資料和token
+		_id: animal._id,
+		name: animal.name,
+		birthday: animal.birthday,
+		bloodType: animal.bloodType,
+		variety: animal.variety,
+		type: animal.type,
+		gender: animal.gender,
+		weight: animal.weight,
+		ligation: animal.ligation,
+	});
+});
+
+export default router;
