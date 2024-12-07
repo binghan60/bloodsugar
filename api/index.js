c09980d867760c0c56e387e9dd5598a6332e2564
binghan <binghan60@gmail.com>
2024-12-07 20:28:08 +0800

    調整血糖取線蓋屏樣式

--- api/index.js
+++ api/index.js
@@ -10,13 +10,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose //連線至資料庫
-	.connect(process.env.MONGODB_URI)
-	.then(() => {
-		console.log('資料庫連線成功');
-	})
-	.catch(() => {
-		console.log('資料庫連線失敗');
-	});
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('資料庫連線成功');
+    })
+    .catch(() => {
+        console.log('資料庫連線失敗');
+    });
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -25,12 +25,12 @@ app.use('/weight', weightRouter);
 app.use('/bloodSugar', bloodSugarRouter);
 
 app.get('/api', (req, res) => {
-	res.send('Hello, Express!');
+    res.send('Hello, Express!');
 });
 const PORT = 3000;
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 export default (req, res) => {
-	app(req, res);
+    app(req, res);
 };
