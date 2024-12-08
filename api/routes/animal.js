1e979edc61fd5bd3cb9db5c2002d67478a0e55c7
binghan <binghan60@gmail.com>
2024-12-08 21:33:08 +0800

    前端新增血糖曲線

--- api/routes/animal.js
+++ api/routes/animal.js
@@ -4,44 +4,42 @@ import Animal from '../models/animalModel.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-	res.send('ANIMAL');
+    res.send('ANIMAL');
 });
 router.get('/:id', async (req, res) => {
-	const user = await Animal.findById(req.params.id); //用網址的ID去資料庫找該用戶  管理者顯示用戶資料用
-	if (user) {
-		res.send(user); //如果有就回傳
-	} else {
-		res.status(404).send({ message: '找不到用戶' });
-	}
+    const user = await Animal.findById(req.params.id); //用網址的ID去資料庫找該用戶  管理者顯示用戶資料用
+    if (user) {
+        res.send(user); //如果有就回傳
+    } else {
+        res.status(404).send({ message: '找不到用戶' });
+    }
 });
 
 router.post('/create', async (req, res) => {
-	try {
-		const newAnimal = new Animal({
-			//以req.body的資料new一個User
-			name: req.body.name,
-			birthday: req.body.birthday,
-			bloodType: req.body.bloodType,
-			variety: req.body.variety,
-			type: req.body.type,
-			gender: req.body.gender,
-			weight: req.body.weight,
-			ligation: req.body.ligation,
-		});
-		const animal = await newAnimal.save(); //保存至資料庫
-		res.send({ animal });
-	} catch (error) {
-		// 捕獲 Mongoose validation 錯誤
-		console.error(error);
+    try {
+        const newAnimal = new Animal({
+            //以req.body的資料new一個User
+            name: req.body.name,
+            birthday: req.body.birthday,
+            bloodType: req.body.bloodType,
+            variety: req.body.variety,
+            type: req.body.type,
+            gender: req.body.gender,
+            weight: req.body.weight,
+            ligation: req.body.ligation,
+        });
+        const animal = await newAnimal.save(); //保存至資料庫
+        res.send({ animal });
+    } catch (error) {
+        // 捕獲 Mongoose validation 錯誤
+        console.error(error);
+        if (error.name === 'ValidationError') {
+            // 如果是驗證錯誤，返回詳細錯誤信息
+            return res.status(400).json({ message: 'Validation Error', errors: error.errors });
+        }
 
-		if (error.name === 'ValidationError') {
-			// 如果是驗證錯誤，返回詳細錯誤信息
-			return res.status(400).json({ message: 'Validation Error', errors: error.errors });
-		}
-
-		// 其他錯誤，返回 500 錯誤
-		res.status(500).json({ message: 'Server Error' });
-	}
+        // 其他錯誤，返回 500 錯誤
+        res.status(500).json({ message: 'Server Error' });
+    }
 });
-
 export default router;
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 ﻿const apipath = 'http://localhost:3000';
 const today = new Date();
 const animalId = '674d5126c36e4fd631d342c6';
+
 const currentDate = {
     year: today.getFullYear(),
     month: today.getMonth(),
@@ -51,6 +52,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         plugins: [ChartDataLabels],
         options: {
             responsive: true,
+            // maintainAspectRatio: false,
             plugins: {
                 legend: {
                     display: false,
@@ -79,7 +81,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             },
         },
     });
-
+    await createCurrentMonthSugarCurve();
     renderCalendar();
 });
 
@@ -98,9 +100,7 @@ async function calendarCells() {
             evening: { bloodSugar: '', insulin: '' },
         };
     });
-
     console.log('days before merge:', days);
-
     // 合并日记数据到 days
     const mergedDays = days.map((day) => {
         const diaryEntry = diaryData.find((entry) => new Date(entry.date).toISOString().split('T')[0] === day.isoDate.split('T')[0]);
@@ -125,7 +125,6 @@ async function calendarCells() {
     while (allDays.length < totalCells) {
         allDays.push({ date: null });
     }
-    console.log(allDays);
     return allDays;
 }
 
@@ -135,7 +134,6 @@ async function renderCalendar() {
     currentMonthDisplay.textContent = `${currentDate.year} 年 ${currentDate.month + 1} 月`;
     calendarGrid.innerHTML = '';
     const cells = await calendarCells();
-    console.log(cells);
     cells.forEach((cell) => {
         const cellDiv = document.createElement('div');
         cellDiv.classList.add(
@@ -147,9 +145,7 @@ async function renderCalendar() {
             'cursor-pointer',
             'm-1' //
         );
-        console.log(cell.month);
-        console.log(currentDate.month + 1);
-        if (currentDate.year === cell.year && currentDate.month + 1 === cell.month && currentDate.day === cell.date) {
+        if (today.getFullYear() === cell.year && today.getMonth() + 1 + 1 === cell.month && today.getDate() === cell.date) {
             cellDiv.classList.remove('bg-blue-100');
             cellDiv.classList.add('bg-sky-300');
         }
@@ -159,13 +155,37 @@ async function renderCalendar() {
 				<!-- 早上 -->
 				<div class="bg-orange-100 p-2 rounded-md mb-2 hover:bg-orange-200">
 					<div class="font-semibold text-orange-500 text-center"><i class="fa-regular fa-sun"></i></div>
-					<div data-type="morningBloodSugar" class="editable p-1 text-sm mt-1 bg-white border border-gray-300 rounded w-full"><i class="fa-solid fa-droplet w-[14px]"></i> : ${cell.morning.bloodSugar ? cell.morning.bloodSugar + 'mg/dl' : '--'}</div>
+					<div data-type="morningBloodSugar" class="editable p-1 text-sm mt-1 bg-white border border-gray-300 rounded w-full"><i class="fa-solid fa-droplet w-[14px]"></i> : ${
+                        cell.morning.bloodSugar
+                            ? `<span class="${(function () {
+                                  if (cell.morning.bloodSugar > 400) {
+                                      return 'text-red-500';
+                                  }
+                                  if (cell.morning.bloodSugar > 250) {
+                                      return 'text-amber-500';
+                                  }
+                                  return 'text-green-500';
+                              })()}">${cell.morning.bloodSugar}</span>` + ' mg/dl'
+                            : '--'
+                    }</div>
 					<div data-type="morningInsulin" class="editable p-1 text-sm mt-1 bg-white border border-gray-300 rounded w-full"><i class="fa-solid fa-syringe"></i> : ${cell.morning.insulin ? cell.morning.insulin + '小格' : '--'}</div>
 				</div>
 				<!-- 晚上 -->
 				<div class="bg-purple-100 p-2 rounded-md hover:bg-purple-200">
 					<div class="font-semibold text-purple-500 text-center"><i class="fa-regular fa-moon"></i></div>
-					<div data-type="eveningBloodSugar" class="editable p-1 text-sm mt-1 bg-white border border-gray-300 rounded w-full"><i class="fa-solid fa-droplet w-[14px]"></i> : ${cell.evening.bloodSugar ? cell.evening.bloodSugar + 'mg/dl' : '--'}</div>
+					<div data-type="eveningBloodSugar" class="editable p-1 text-sm mt-1 bg-white border border-gray-300 rounded w-full"><i class="fa-solid fa-droplet w-[14px]"></i> : ${
+                        cell.evening.bloodSugar
+                            ? `<span class="${(function () {
+                                  if (cell.evening.bloodSugar > 400) {
+                                      return 'text-red-600';
+                                  }
+                                  if (cell.evening.bloodSugar > 250) {
+                                      return 'text-amber-500';
+                                  }
+                                  return 'text-green-500';
+                              })()}">${cell.evening.bloodSugar}</span>` + ' mg/dl'
+                            : '--'
+                    }</div>
 					<div data-type="eveningInsulin" class="editable p-1 text-sm mt-1 bg-white border border-gray-300 rounded w-full"><i class="fa-solid fa-syringe"></i> : ${cell.evening.insulin ? cell.evening.insulin + '小格' : '--'}</div>
 				</div>`;
             calendarGrid.appendChild(cellDiv);
@@ -211,8 +231,8 @@ async function renderCalendar() {
                         // 血糖input
                         const input = document.createElement('input');
                         input.type = 'number';
-                        input.value = currentValue.replace('mg/dl', '') === '--' ? '' : currentValue.replace('mg/dl', '');
-
+                        input.value = currentValue.replace(' mg/dl', '') === '--' ? '' : currentValue.replace(' mg/dl', '');
+                        console.log(currentValue);
                         input.className = 'p-1 mt-1 border border-blue-500 rounded text-sm w-full';
                         target.replaceWith(input);
                         input.focus();
@@ -220,11 +240,35 @@ async function renderCalendar() {
                             input.replaceWith(target);
                             if (target.dataset.type.includes('morning')) {
                                 const insertBloodSugarResponse = await createDiaryData(formattedDate, input.value, '', '', '', '');
-                                target.innerHTML = `<i class="fa-solid fa-droplet"></i> : ${insertBloodSugarResponse.morning.bloodSugar ? insertBloodSugarResponse.morning.bloodSugar + 'mg/dl' : '--'}`;
+                                target.innerHTML = `<i class="fa-solid fa-droplet"></i> : ${
+                                    insertBloodSugarResponse.morning.bloodSugar
+                                        ? `<span class="${(function () {
+                                              if (insertBloodSugarResponse.morning.bloodSugar > 400) {
+                                                  return 'text-red-600';
+                                              }
+                                              if (insertBloodSugarResponse.morning.bloodSugar > 250) {
+                                                  return 'text-amber-500';
+                                              }
+                                              return 'text-green-500';
+                                          })()}">${insertBloodSugarResponse.morning.bloodSugar}</span>` + ' mg/dl'
+                                        : '--'
+                                }`;
                             }
                             if (target.dataset.type.includes('evening')) {
                                 const insertBloodSugarResponse = await createDiaryData(formattedDate, '', '', input.value, '', '');
-                                target.innerHTML = `<i class="fa-solid fa-droplet"></i> : ${insertBloodSugarResponse.evening.bloodSugar ? insertBloodSugarResponse.evening.bloodSugar + 'mg/dl' : '--'}`;
+                                target.innerHTML = `<i class="fa-solid fa-droplet"></i> : ${
+                                    insertBloodSugarResponse.evening.bloodSugar
+                                        ? `<span class="${(function () {
+                                              if (insertBloodSugarResponse.evening.bloodSugar > 400) {
+                                                  return 'text-red-600';
+                                              }
+                                              if (insertBloodSugarResponse.evening.bloodSugar > 250) {
+                                                  return 'text-amber-500';
+                                              }
+                                              return 'text-green-500';
+                                          })()}">${insertBloodSugarResponse.evening.bloodSugar}</span>` + ' mg/dl'
+                                        : '--'
+                                }`;
                             }
                         });
                     }
@@ -260,21 +304,132 @@ document.getElementById('nextMonth').addEventListener('click', () => {
 
 document.getElementById('add-field-btn').addEventListener('click', () => {
     const container = document.getElementById('input-container');
-
     const newFieldGroup = document.createElement('div');
     newFieldGroup.classList.add('grid', 'grid-cols-[2fr_2fr_0.5fr]', 'gap-4', 'items-center', 'border', 'p-2', 'rounded-md', 'shadow-md');
     newFieldGroup.innerHTML = `
-        <input type="time" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-300" />
-        <input type="number" placeholder="輸入血糖值" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-300" />
+        <input type="time" name="sugarCurveTime" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-300" />
+        <input type="number" name="sugarCurveBloodSugar" placeholder="輸入血糖值" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-300" />
         <button class="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-2 rounded-md">X</button>
     `;
-
     container.appendChild(newFieldGroup);
-
     newFieldGroup.querySelector('button').addEventListener('click', () => {
         newFieldGroup.remove();
     });
 });
+
+async function submitSugarCurve(date) {
+    let timeArray = [];
+    let sugarArray = [];
+    document.querySelectorAll('input[name=sugarCurveTime]').forEach((x) => {
+        timeArray.push(x.value);
+    });
+    document.querySelectorAll('input[name=sugarCurveBloodSugar]').forEach((x) => {
+        sugarArray.push(x.value);
+    });
+    if (timeArray.includes('') || sugarArray.includes('')) {
+        alert('請輸入完整資訊');
+        return;
+    }
+    let records = timeArray.map((time, index) => ({
+        time: time,
+        value: sugarArray[index],
+    }));
+
+    console.log({ records });
+    const createSugarCurveResponse = await createSugarCurve(date, records);
+    console.log(createSugarCurveResponse);
+    if (!createSugarCurveResponse._id) {
+        alert('新增失敗');
+    }
+    await createCurrentMonthSugarCurve();
+    document.querySelector('#fade').style.display = 'none';
+}
+
+function openCreateSugarCurveWindow() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+    document.querySelector('#sugarCurveYear').value = year;
+    document.querySelector('#sugarCurveMonth').value = month;
+    document.querySelector('#sugarCurveDay').value = day;
+    document.querySelector('#sugarCurveBtn').innerHTML = `
+        <button class="bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-6 rounded-lg shadow-md w-1/3 transition-all" onclick="document.querySelector('#fade').style.display='none'">取消</button>
+        <button class="bg-blue-500 hover:bg-blue-400 text-white py-2 px-6 rounded-lg shadow-md w-1/3 transition-all" onclick="submitSugarCurve('${formattedDate}')">確定</button>`;
+
+    document.querySelector('#fade').style.display = 'flex';
+}
+async function createCurrentMonthSugarCurve() {
+    const sugarCurve = await getBloodSugarCurve();
+    document.querySelector('#monthChart').innerHTML = '';
+    sugarCurve.forEach((data, index) => {
+        const canvas = document.createElement('canvas');
+        canvas.id = `sugarCurvechart-${index}`; // 设置唯一ID
+        canvas.width = '100%';
+        canvas.height = '100%';
+        const div = document.createElement('div');
+        div.classList.add('rounded-lg', 'overflow-hidden', 'shadow-lg', 'bg-white', 'mt-6', 'p-4', 'h-[350px]');
+        document.querySelector('#monthChart').appendChild(div);
+        div.append(canvas);
+        const timeArray = data.records.map((x) => x.time);
+        const sugarArray = data.records.map((x) => x.value * 1);
+        const chartDate = new Date(data.date).toLocaleString().slice(0, 10);
+        console.log(chartDate);
+        new Chart(canvas, {
+            type: 'line',
+            data: {
+                labels: timeArray,
+                datasets: [
+                    {
+                        label: '血糖',
+                        data: sugarArray,
+                        borderColor: '#D2E9FF',
+                        backgroundColor: '#D2E9FF',
+                        tension: 0,
+                        pointRadius: 8,
+                        pointHoverRadius: 15,
+                    },
+                ],
+            },
+            plugins: [ChartDataLabels],
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: `${chartDate}血糖曲線`,
+                    },
+                    legend: {
+                        display: false,
+                        position: 'top',
+                    },
+                    datalabels: {
+                        display: true,
+                        color: '#0080FF',
+                        font: {
+                            weight: 'bold',
+                            size: 16,
+                        },
+                        align: 'top',
+                        formatter: (value) => value.toFixed(1),
+                    },
+                },
+                scales: {
+                    y: {
+                        title: {
+                            display: true,
+                            text: '體重',
+                        },
+                        beginAtZero: false,
+                        max: Math.max(...sugarArray) * 1.1,
+                    },
+                },
+            },
+        });
+    });
+}
 
 async function getAnimalProfile() {
     try {
@@ -324,6 +479,22 @@ async function getDiaryData() {
         throw error;
     }
 }
+async function getBloodSugarCurve() {
+    try {
+        const response = await fetch(`${apipath}/bloodSugar/getCurve?userId=${animalId}&year=${currentDate.year}&month=${currentDate.month + 1}`, {
+            method: 'GET',
+        });
+        if (!response.ok) {
+            throw new Error(`Request failed with status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        alert('伺服器忙碌中，請稍後再試。');
+        console.error('createDiaryData', error);
+        throw error;
+    }
+}
 
 async function createDiaryData(date, morningBloodSugar, morningInsulin, eveningBloodSugar, eveningInsulin, notes, isoDate) {
     try {
@@ -345,6 +516,32 @@ async function createDiaryData(date, morningBloodSugar, morningInsulin, eveningB
                 },
                 notes,
                 isoDate,
+            }),
+        });
+        if (!response.ok) {
+            throw new Error(`Request failed with status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        alert('伺服器忙碌中，請稍後再試。');
+        console.error('createDiaryData', error);
+        throw error;
+    }
+}
+
+async function createSugarCurve(date, records) {
+    try {
+        const response = await fetch(`${apipath}/bloodSugar/createCurve`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userId: animalId,
+                date,
+                records,
             }),
         });
         if (!response.ok) {
