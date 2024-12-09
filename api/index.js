f53ec362c0824747e8b9245986d2220817a123bb
binghan60 <binghan60@gmail.com>
2024-12-09 12:16:14 +0800

    增加資料庫錯誤訊息

--- api/index.js
+++ api/index.js
@@ -8,14 +8,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 mongoose //連線至資料庫
     .connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('資料庫連線成功');
     })
-    .catch(() => {
+    .catch((err) => {
         console.log('資料庫連線失敗');
+        console.log(err.message);
     });
 const app = express();
 app.use(bodyParser.json());
