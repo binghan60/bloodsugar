2b6351a76afc45d53b21cc16471e5368c2e6b73d
binghan60 <binghan60@gmail.com>
2024-12-10 15:29:33 +0800

    體重API排序

--- server/models/weightModel.js
+++ server/models/weightModel.js
@@ -1,12 +1,12 @@
 ﻿import mongoose from 'mongoose';
 
 const weightSchema = new mongoose.Schema(
-	{
-		userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Animal', required: true, index: true },
-		date: { type: Date, required: true },
-		weight: { type: Number, required: true },
-	},
-	{ timestamps: true }
+    {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Animal', required: true, index: true },
+        date: { type: Date, required: true },
+        weight: { type: Number, required: true },
+    },
+    { timestamps: true }
 );
 const Weight = mongoose.model('Weight', weightSchema);
 
