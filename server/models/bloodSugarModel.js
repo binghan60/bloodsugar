fa295de8a97e8177d9ab3b89bce70a6260cd7b5e
binghan <binghan60@gmail.com>
2024-12-28 21:10:17 +0800

    server:新增早晚血糖時間

--- server/models/bloodSugarModel.js
+++ server/models/bloodSugarModel.js
@@ -1,35 +1,37 @@
 ﻿import mongoose from 'mongoose';
 
 const bloodSugarSchema = new mongoose.Schema(
-	{
-		userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Animal', required: true, index: true },
-		date: {
-			type: Date,
-			required: true,
-			unique: true,
-		},
-		morning: {
-			bloodSugar: {
-				type: Number,
-			},
-			insulin: {
-				type: Number,
-			},
-		},
-		evening: {
-			bloodSugar: {
-				type: Number,
-			},
-			insulin: {
-				type: Number,
-			},
-		},
-		notes: {
-			type: String,
-			default: '',
-		},
-	},
-	{ timestamps: true }
+    {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Animal', required: true, index: true },
+        date: {
+            type: Date,
+            required: true,
+            unique: true,
+        },
+        morning: {
+            time: { type: String },
+            bloodSugar: {
+                type: Number,
+            },
+            insulin: {
+                type: Number,
+            },
+        },
+        evening: {
+            time: { type: String },
+            bloodSugar: {
+                type: Number,
+            },
+            insulin: {
+                type: Number,
+            },
+        },
+        notes: {
+            type: String,
+            default: '',
+        },
+    },
+    { timestamps: true }
 );
 
 const BloodSugarSchema = mongoose.model('Record', bloodSugarSchema);
